0ac870d8afc783d41f74f90c70b535e4aa5642b3
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-02-07 14:31:46 +0100

    haciendo sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+//modulos
+import { PaisesModule } from './paises/paises.module';
+import { SharedModule } from './shared/shared.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PaisesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
