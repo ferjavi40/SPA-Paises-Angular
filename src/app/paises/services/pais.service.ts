a333135b5e714506e2b67bdcbf9f1818dfcef1e7
Fernando Ramirez <ferjaviramirez@gmail.com>
2021-02-10 14:42:29 +0100

    tarea hecha

--- src/app/paises/services/pais.service.ts
+++ src/app/paises/services/pais.service.ts
@@ -14,11 +14,15 @@ export class PaisService {
   constructor(private http: HttpClient) { }
 
 
-  buscarPais(termino: string): Observable<Country[]> {
-
+  buscarPais(termino: string): Observable<Country []> {
     const url = `${ this.apiUrl }/name/${ termino }`;
     return this.http.get<Country[]>( url);
     
+  }
+
+  buscarCapital ( termino:string ): Observable<Country []> {
+    const url = `${ this.apiUrl }/capital/${termino}`;
+    return this.http.get<Country []>( url );
   }
 
 }
